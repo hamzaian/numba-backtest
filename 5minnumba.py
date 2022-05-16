18e735cd547ec5259fb23d680a2b995afab0cb8e
hamzaian <hamzah.sadiq@gmail.com>
2022-05-16 10:42:19 -0700

    mvp complete

--- 5minnumba.py
+++ 5minnumba.py
@@ -1,3 +1,5 @@
+from curses import tparm
+from importlib.metadata import entry_points
 import pandas as pd
 import utils
 import plotly.graph_objects as go
@@ -24,7 +26,38 @@ while True:
         break
 
 # data_load.shape = (913,30)
-print(data_load.shape[0]) # = 913 rows
+print(data_load.shape) # [0] = 913 rows
+
+data_load.resize(data_load.shape[0], (data_load.shape[1]+7))
+print(data_load.shape) 
+
+data_load[:,30] = 0
+data_load[:,31] = 0
+data_load[:,34] = 0
+data_load[:,35] = 0
+
+data_load[:,32] = -1
+data_load[:,33] = -1
+
+data_load[:,36] = -1
+
+
+
+for i in range(455,463):
+    print(data_load[i])
+print(data_load[461])
+column_names = ['0. index', '1. timestamp', '2. volume', 
+'3. mid_o',    '4. mid_h',    '5. mid_l', '6. mid_c', '7. ask_h', '8. ask_l', '9. ask_c', '10. bid_h', '11. bid_l', '12. bid_c', 
+'13. ap', '14. esa', '15. d', '16. ci', '17. tci', '18. wt1', '19. wt2', '20. EMA_9', '21. high'  '22. low', 
+'23. wt2_prev', '24. DIFF',  '25. IS_TRADE_prev', '26. IS_TRADE ', '27. ENTRY', '28. STOPLOSS', '29. TAKEPROFIT'];
+
+extra_columns = ['30. long P/L %', '31. short P/L %', '32. opened (index)', '33. closed(index)',    
+'34. close P/L%',    '35. duration', '36. EXIT'];
+#don't forget to add null values to all the extra_columns
+# [23-30]
+#row[n] to denote a cell when applying row-wise function 
+
+
 
 global is_running
 is_running = 0
@@ -34,36 +67,91 @@ is_running = 0
 #in the end, extract columns with non-null P/L values
 global opened
 global stopped
+stopped = -1
+
+global entry
+global tp
+global sl
+tp = -1
+sl = -1 
+entry = -1
+
+global trade_num
+trade_num = 0
+
 #make range start at 1 instead of 0 if you need previous row for some operation
 
 #using candle index as proxy for duration at the moment
-for i in range((data_load.shape[0] + 1)): 
-    is_trade = data_load[i][25]
-    if (is_trade != 0 ):
-        is_running = is_trade
-    if(is_running == 1):
-        if()
-    elif(is_running == -1):
 
-        #print(is_running)
-    # if (is_trade == (-is_running)):
-    #     is_running = False
+''' optimal way of applying numpy functions, should be able to do intercolumn functions too so long as same row?
+x = np.array([1, 2, 3, 4, 5])
+f = lambda x: x ** 2
+squares = f(x)'''
 
-    pass
+# data_load
+@jit
+def backtesting():
+    for i in range((data_load.shape[0])): 
+        is_trade = data_load[i][25]
+        long = ( (data_load[i][10]/entry)-1 )*100
+        short = ( (data_load[i][8]/entry)-1 )*100
+        #row.ask_l-self.entry)/self.entry)*100  <= -0.25
+        if (is_trade != 0 ):
+            # if(stopped == -1):
+            #     stopped = data_load[i][0]
+            #     if(is_running == 1):
+            #         if():     #row.ask_l-self.entry)/self.entry)*100
+            #     elif(is_running == -1):
+            #         if():
 
-# for i in range(455,463):
-#     print(data_load[i])
-# print(data_load[461])
-column_names = ['0. index', '1. timestamp', '2. volume', 
-'3. mid_o',    '4. mid_h',    '5. mid_l', '6. mid_c', '7. ask_h', '8. ask_l', '9. ask_c', '10. bid_h', '11. bid_l', '12. bid_c', 
-'13. ap', '14. esa', '15. d', '16. ci', '17. tci', '18. wt1', '19. wt2', '20. EMA_9', '21. high'  '22. low', 
-'23. wt2_prev', '24. DIFF',  '25. IS_TRADE_prev', '26. IS_TRADE ', '27. ENTRY', '28. STOPLOSS', '29. TAKEPROFIT'];
+            is_running = is_trade
+            tp = data_load[i][29]
+            sl = data_load[i][28]
+            entry = data_load[i][27]
 
-extra_columns = ['0. index', '1. timestamp', '2. volume', '3. mid_o',    '4. mid_h',    '5. mid_l', '6. mid_c', '7. ask_h']
-# [23-30]
-#row[n] to denote a cell when applying row-wise function 
+            opened = data_load[i][0]
+
+            trade_num += 1
+
+        if(is_running == 1):
+            if(long >= .25 or data_load[i][10] <= sl or data_load[i][10] >= tp):
+                is_running = 0
+                stopped = data_load[i][0]
+                data_load[i][35] = stopped - opened
+                data_load[i][27] = entry 
+                data_load[i][36] = data_load[i][10]
+                data_load[i][34] = ((data_load[i][data_load][36]/entry) - 1)*100
+
+                opened = -1
+                stopped = -1
+            
+
+        elif(is_running == -1):
+            if(short <= -.25 or data_load[i][8] >= sl or data_load[i][8] <= tp):
+                is_running = 0
+                stopped = data_load[i][0]
+                data_load[i][32] = opened
+                data_load[i][33] = stopped
+
+                data_load[i][35] = stopped - opened
+                data_load[i][27] = entry 
+                data_load[i][36] = data_load[i][8]
+                data_load[i][34] = (1 - (data_load[i][data_load][36]/entry))*100
 
 
+                opened = -1
+                stopped = -1
+            #print(is_running)
+        # if (is_trade == (-is_running)):
+        #     is_running = False
+
+# leverage = 20
+# #sum column 34 and divide by trade_num and multiply by leverage
+# avg_profit = 
+# #just don't divide by ttrade_num`
+# total_profit = 
+# #sum duration and divide by trade_num 
+# avg_duration = 
 '''
 def my_func(a):
 
